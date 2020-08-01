03f8bc13753a168a8949861ac41eafbe1ec76406
Adrian <farcasadrian94@gmail.com>
2020-08-01 22:08:04 +0300

    main-navbar-design

--- frontend/src/app/angular-material.module.ts
+++ frontend/src/app/angular-material.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from "@angular/material/card";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatButtonModule} from "@angular/material/button";
+import {MatTooltipModule} from "@angular/material/tooltip";
 
 
 @NgModule({
   imports: [
     CommonModule,
     MatCardModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatTooltipModule,
   ],
   exports: [
     MatCardModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatTooltipModule,
   ],
   providers: []
 })
---
+++ frontend/src/app/components/main-navbar-content/main-navbar-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-navbar-content',
+  templateUrl: './main-navbar-content.component.html',
+  styleUrls: ['./main-navbar-content.component.css']
+})
+export class MainNavbarContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

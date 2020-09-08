5b1ba2f69a8e8d5c501f713bb90c5325f300bcae
Adrian <farcasadrian94@gmail.com>
2020-09-08 21:36:50 +0300

    ignore idea directory

--- frontend/src/app/environments/enviroments.ts
+++ frontend/src/app/environments/enviroments.ts
@@ -1,3 +1,3 @@
 export const proxyLink = 'https://cors-anywhere.herokuapp.com/';
 export const apiKey = 'AIzaSyDxKTQhP9FucPUPnJvG1yqSXt6L9NKnUSI';
-export const serverAPI = 'http://localhost:8700';
+export const serverAPI = 'http://localhost:8000';
